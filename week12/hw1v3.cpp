9abbba31750ec5992ffc95de71d855a8b526b302
Khit Khom <khomkhit460@gmail.com>
2025-02-18 22:26:35 +0700

    week12 hw1v3

---
+++ week12/hw1v3.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+/* number 1 : input 
+	2 : output
+	3 : End
+	else Wrong option
+	*/
+
+int ops;
+
+struct Student
+{
+    int id, age;
+    char gender;
+    string name , contact ;
+    float score;
+};
+
+
+vector<Student> students;
+    void input(){
+        int index = 0;
+        Student student;
+        cout << "\t>>>>>Input Student Information<<<<<\n";
+        cout << "\tStudent ID : ";
+        cin >> student.id;
+        cout << "\tName : ";
+        cin >> student.name;
+        cout << "\tGender : ";
+        cin >> student.gender;
+        cout << "\tAge : ";
+        cin >> student.age;
+        cout << "\tScore : ";
+        cin >> student.score;
+        cout << "\tContact : ";
+        cin >> student.contact;
+
+        students.push_back(student);
+    }
+    void output(){
+        cout << "\t>>>>>Output Student Information<<<<<\n";
+        cout << setw(5) << "ID" << setw(15) << "Name" << setw(10) << "Gender" << setw(5) << "Age" << setw(10) << "Score" << setw(20) << "Contact" << endl;
+        cout << "---------------------------------------------------------------" << endl;
+        for (const auto& student : students) {
+            cout << setw(5) << student.id << setw(15) << student.name << setw(10) << student.gender << setw(5) << student.age << setw(10) << student.score << setw(20) << student.contact << endl;
+        }
+        cout << "---------------------------------------------------------------" << endl;
+    }
+
+int main() {
+    do {
+        cout << "\nPlease Choose a number : \n1 for input. \n2 for output \n3 for end.\n"<< endl;
+        cin >> ops;
+        switch (ops) {
+            case 1:
+                input();
+                break;
+            case 2:
+                output();
+                break;
+            case 3:
+                break;
+            default :
+                cout << "Wrong Option !!"<<endl;
+        }
+    } while (ops != 3);
+    return 0;
+}

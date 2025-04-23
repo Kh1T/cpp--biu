7ed3b921492024693a9ed3e0a592bbeecf491cbd
Khit Khom <khomkhit460@gmail.com>
2025-04-23 18:18:18 +0700

    add: implement basic array output program in C++

---
+++ semester2/week2/week2.cpp
@@ -0,0 +1,13 @@
+// Array 
+#include <iostream>
+using namespace std;
+
+int main () {
+    int list[5]= {1,2,3,4,5};
+
+    for (int i=0; i<5; i++){
+        cout << list[i] << endl;
+    }
+
+    return 0;
+}
